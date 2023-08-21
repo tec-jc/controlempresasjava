b0cda55902d4c8a227757b503befa01cc0bb4425
JC Tula <jtula_esfe@outlook.es>
2023-08-21 13:54:38 -0600

    Codificación de las clases del paquete Utils de AppWeb

--- ControlEmpresas.AppWeb/src/java/controlempresas/appweb/utils/IAuthorize.java
+++ ControlEmpresas.AppWeb/src/java/controlempresas/appweb/utils/IAuthorize.java
@@ -1,5 +1,8 @@
 package controlempresas.appweb.utils;
 
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+
 public interface IAuthorize {
-    
+    void authorize() throws ServletException, IOException;
 }
