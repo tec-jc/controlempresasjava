9f4480969834484dc83def9b1d79069083f227d2
JC Tula <jtula_esfe@outlook.es>
2023-08-28 12:58:29 -0600

    Codificación de las vistas

--- ControlEmpresas.AppWeb/src/java/controlempresas/appweb/controllers/ContactoServlet.java
+++ ControlEmpresas.AppWeb/src/java/controlempresas/appweb/controllers/ContactoServlet.java
@@ -1,7 +1,6 @@
 package controlempresas.appweb.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
